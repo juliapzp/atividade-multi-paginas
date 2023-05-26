b89e403aab11e26bdfdd62272762d625c37c66af
juliapzp <julyanap36@gmail.com>
2023-05-26 10:22:34 -0300

    initial commit

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ function converte(){
         resultado = (temperaturainicial * 1.8) + 32
     }
     else if(document.getElementById("fc").checked){
-        resultado = (temperaturainicial - 32) / 1.8
+        resultado = (temperaturainicial - 32) * 5/9
     }
     else if(document.getElementById("ck").checked){
-        resultado = temperaturainicial + 273
+        resultado = Number(temperaturainicial + 273.15)
     }
     else if(document.getElementById("kc").checked){
-        resultado = temperaturainicial - 273
+        resultado = Number(temperaturainicial - 273)
     }
     else if(document.getElementById("fk").checked){
-        resultado = ((temperaturainicial - 32) / 1.8) + 273
+        resultado = ((temperaturainicial - 32) * 5/9) + 273.15
     }
     else if(document.getElementById("kf").checked){
-        resultado = ((temperaturainicial - 273) * 1.8 + 32)
+        resultado = ((temperaturainicial - 273.15) * 9/5) + 32
     }
 
     document.getElementById("resultado").innerHTML = resultado
